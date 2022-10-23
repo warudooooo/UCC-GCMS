178cfd1859625fed807c72ccfe317e8f76816040
warudooooo <waldoreyes2301@gmail.com>
2022-10-23 18:16:06 +0800

    Able to add course

--- adminPortal/course.php
+++ adminPortal/course.php
@@ -14,13 +14,37 @@ if (mysqli_num_rows($query) > 0) {
 
     $sName = $row['studentName'];
 }
+
+if (isset($_POST['submit'])) {
+    $cCode = $_POST['cCode'];
+    $cName = $_POST['cName'];
+    //Sanitize
+    $cCode = $mysqli->real_escape_string($cCode);
+    $cName = $mysqli->real_escape_string($cName);
+
+    // if (mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM tbl_courselist WHERE courseCode='{$cCode}'")) > 0) {
+    // 	$msg = "<div class='eml' style='margin-bottom: 10px; margin-top: -20px;'>This email adress is already in use. Please use a different one.</div>";
+    // } else if (mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM tbl_courselist WHERE courseName='{$cName}'")) > 0) {
+    // 	$msg = "<div class='eml' style='margin-bottom: -5px; margin-top: -20px;'>This Student Number already exists.</div>";
+    // } else {
+    //Insert to DB
+
+    $sql = "INSERT INTO tbl_courselist(courseCode,courseName) VALUES('$cCode','$cName')";
+    $result = mysqli_query($mysqli, $sql);
+    // }
+}
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
+
 <head>
     <link rel="icon" href="src/images/uccLogo.png">
 </head>
-<?php include 'includes/header.php'?>
+<?php include 'includes/header.php' ?>
 
 <body>
     <!-- ============================================================== -->
@@ -35,11 +59,10 @@ if (mysqli_num_rows($query) > 0) {
     <!-- ============================================================== -->
     <!-- Main wrapper - style you can find in pages.scss -->
     <!-- ============================================================== -->
-    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
-        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
 
-        <?php include 'includes/topbar.php'?>
-        <?php include 'includes/sidebar.php'?>
+        <?php include 'includes/topbar.php' ?>
+        <?php include 'includes/sidebar.php' ?>
 
         <!-- ============================================================== -->
         <!-- Page wrapper  -->
@@ -73,82 +96,106 @@ if (mysqli_num_rows($query) > 0) {
                 <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="table-responsive">
-                            <a class="btn btn-sm elevation-2" href="#" data-toggle="modal"
-                        data-target="#add" style="padding: 0.5rem; margin-top: 20px;margin-left: 10px;background-color: #f4845f; color: #fff;"><i
-                        class="fa fa-plus"></i> Add New</a>
-                        <table id="example" class="table table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Course Code</th>
-                                            <th scope="col">Course Complete Name</th>
-                                            <th scope="col">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>BSIS</td>
-                                            <td>Information System</td>
-                                            <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
-                                            <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>BSIT</td>
-                                            <td>Information Technology</td>
-                                            <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
-                                            <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>BSCS</td>
-                                            <td>Computer Science</td>
-                                            <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
-                                            <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>BSEMC</td>
-                                            <td>Entertainment and Multimedia Computing</td>
-                                            <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
-                                            <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>BSTM</td>
-                                            <td>Tourism Management</td>
-                                            <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
-                                            <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                <form method="POST" action="" autocomplete="off">
+                    <div class="row">
+                        <div class="col-lg-12 col-xlg-12 col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form class="form-horizontal form-material mx-2">
+                                        <div class="form-group">
+                                            <label class="col-md-12">Course Code</label>
+                                            <div class="col-md-12">
+                                                <textarea rows="1" class="form-control form-control-line" name="cCode"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-md-12">Course Complete Name</label>
+                                            <div class="col-md-12">
+                                                <textarea rows="1" class="form-control form-control-line" name="cName"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-12">
+                                                <button class="btn btn-success text-white" style="background-color: #f25c54 !important; border: none;" name="submit" type="submit">Add New Course</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                </form>
+                <div class="col-12">
+                <div class="card">
+                    <div class="table-responsive">
+                        <table id="example" class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Course Code</th>
+                                    <th scope="col">Course Complete Name</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>BSIS</td>
+                                    <td>Information System</td>
+                                    <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
+                                        <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>BSIT</td>
+                                    <td>Information Technology</td>
+                                    <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
+                                        <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>BSCS</td>
+                                    <td>Computer Science</td>
+                                    <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
+                                        <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>BSEMC</td>
+                                    <td>Entertainment and Multimedia Computing</td>
+                                    <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
+                                        <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>BSTM</td>
+                                    <td>Tourism Management</td>
+                                    <td><a href="javascript:void(0)"><i class="fa fa-edit" style="color: #f4845f;"></i></a>
+                                        <a href="javascript:void(0)"><i class="fa fa-trash" style="color: #f4845f;"></i></a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                <!-- ============================================================== -->
-                <!-- End PAge Content -->
-                <!-- ============================================================== -->
-                <!-- ============================================================== -->
             </div>
-            <!-- ============================================================== -->
-            <!-- End Container fluid  -->
-            <!-- ============================================================== -->
         </div>
         <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
+        <!-- End PAge Content -->
         <!-- ============================================================== -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Container fluid  -->
+    <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Page wrapper  -->
+    <!-- ============================================================== -->
     </div>
     <!-- ============================================================== -->
     <!-- End Wrapper -->
     <!-- ============================================================== -->
     <!-- ============================================================== -->
 
-    <?php include 'includes/footer.php'?>
+    <?php include 'includes/footer.php' ?>
 
 </body>
 
