cf71f42bf3211f9589ab8b78420d044c5de6f684
warudooooo <waldoreyes2301@gmail.com>
2023-05-11 12:43:07 +0800

    added connection error

--- config.php
+++ config.php
@@ -5,4 +5,9 @@ $password = "";
 $database = "db_guidance";
 
 $mysqli = mysqli_connect($server,$username,$password,$database);
+
+if ($mysqli->connect_errno != 0){
+    echo $mysqli->connect_error;
+    exit();
+}
 ?>
