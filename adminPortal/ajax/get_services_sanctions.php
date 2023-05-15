fd2c8d96134e7bd5962e0b5dc5d5ab06a870c59b
warudooooo <waldoreyes2301@gmail.com>
2023-05-15 14:41:29 +0800

    remove comment

--- adminPortal/ajax/get_services_sanctions.php
+++ adminPortal/ajax/get_services_sanctions.php
@@ -4,7 +4,6 @@ include '../../config.php';
 $selectedCase = $_GET['case'];
 $sNumber = $_GET['sNumber'];
 
-// Query the database to fetch the appropriate $selectCount based on the selected case
 $query = mysqli_query($mysqli, "SELECT * FROM tbl_sanctioncounter WHERE studentNumber = '$sNumber' AND caseDetails = '$selectedCase'");
 $row = mysqli_fetch_array($query);
 if (mysqli_num_rows($query) === 1) {
