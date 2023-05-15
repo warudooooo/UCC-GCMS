e298fc1142373ca21f01e585b1f441cb0a0cf31e
warudooooo <71481737+warudooooo@users.noreply.github.com>
2023-05-15 15:31:41 +0800

    Delete config(hosting).php

--- config(hosting).php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "localhost";
-$username = "id19586514_guidance";
-$password = "Chuckn0rr1s_";
-$database = "id19586514_db_guidance";
-
-$mysqli = mysqli_connect($server,$username,$password,$database);
-?>
